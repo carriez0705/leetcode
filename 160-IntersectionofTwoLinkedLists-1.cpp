fa3cd45c178c8640a21a5abdef5c4dec0f8d9f77
carriez0705 <carriez0705@gmail.com>
2015-07-18 22:51:59 +0800

    Create 160-IntersectionofTwoLinkedLists-1.cpp

---
+++ 160-IntersectionofTwoLinkedLists-1.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int m = 1, n = 1;
+        if(!headA||!headB) return NULL;
+        ListNode * ptr1=headA, *ptr2=headB;
+        while(ptr1->next){
+            m++;
+            ptr1 = ptr1->next;
+        }
+        while(ptr2->next){
+            n++;
+            ptr2 = ptr2->next;
+        }
+        if(ptr1!=ptr2) return NULL;
+        ptr1 = headA;
+        ptr2 = headB;
+        if(m>n){
+            int r = m-n, i = 0;
+            while(i<r){
+               ptr1 = ptr1->next; 
+               i++;
+            }
+        }
+        else{
+            int r = n-m, i = 0;
+            while(i<r){
+                ptr2= ptr2->next;
+                i++;
+            }
+        }
+        while(ptr1!=ptr2){
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        return ptr1;
+    }
+};
