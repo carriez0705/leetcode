5b99ee8769cec292b72b683811a1133b47dea16a
carriez0705 <carriez0705@gmail.com>
2015-07-28 23:34:37 +0800

    Create 198-HouseRobber.cpp

---
+++ 198-HouseRobber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(!nums.size()) return 0;
+        vector<vector<int> > vec(nums.size(), vector<int>(2,0));
+        vec[0][0] = 0;
+        vec[0][1] = nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            vec[i][0] = max(vec[i-1][0], vec[i-1][1]);
+            vec[i][1] = vec[i-1][0]+nums[i];
+        }
+        return max(vec[nums.size()-1][0], vec[nums.size()-1][1]);
+    }
+};
