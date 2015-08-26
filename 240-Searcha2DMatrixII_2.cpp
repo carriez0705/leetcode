08bdc0818cf4a99ca57b366427e773b9a877bffd
carriez0705 <carriez0705@gmail.com>
2015-08-26 17:29:10 +0800

    Update 240-Searcha2DMatrixII_2.cpp

--- 240-Searcha2DMatrixII_2.cpp
+++ 240-Searcha2DMatrixII_2.cpp
@@ -3,11 +3,11 @@ public:
     
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if(!matrix.size()) return false;
-        int x = 0, y = matrix[0].size()-1;
-        while(x<matrix.size() && y>=0){
+        int x = matrix.size()-1, y = 0;
+        while(x>=0 && y<matrix[0].size()){
             if(matrix[x][y]==target) return true;
-            else if(matrix[x][y]>target) y--;
-            else x++;
+            else if(matrix[x][y]>target) x--;
+            else y++;
         }
         return false;
     }
