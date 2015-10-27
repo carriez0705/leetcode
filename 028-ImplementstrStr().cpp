3c4f8ca4e7900d44e68ef4adf4b17e27fd7a7f85
carriez0705 <carriez0705@gmail.com>
2015-10-27 23:03:21 +0800

    Update 028-ImplementstrStr().cpp

--- 028-ImplementstrStr().cpp
+++ 028-ImplementstrStr().cpp
@@ -19,3 +19,35 @@ public:
         return -1;
     }
 };
+
+//solution 2- pattern search
+//not suitable for this one- needle search, O(n) space complexity
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(!needle.size()) return 0;
+        if(!haystack.size() || haystack.size()<needle.size()) return -1;
+        int N = 256;
+        vector<vector<int> > dfa(N, vector<int>(needle.size(),0));
+        for(int j = 0; j<N; j++){
+            dfa[j][0] = 0;
+        }
+        dfa[needle[0]][0] = 1;
+        int X = 0;
+        for(int i = 1; i<needle.size(); i++){
+            for(int j = 0; j<N; j++)
+                dfa[j][i] = dfa[j][X];
+            dfa[needle[i]][i] = i+1;
+            X = dfa[needle[i]][X];
+        }
+        int k = 0;
+        for(int i = 0; i<haystack.size(); i++){
+            k = dfa[haystack[i]][k];
+            if(k==needle.size())
+                return i-k+1;
+            if(haystack.size()-i<=needle.size()-k)
+                break;
+        }
+        return -1;
+    }
+};
