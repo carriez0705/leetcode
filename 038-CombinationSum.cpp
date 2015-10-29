4398aaa3540501d99fed05e19d1787c7afee6187
carriez0705 <carriez0705@gmail.com>
2015-10-29 23:21:40 +0800

    Create 038-CombinationSum.cpp

---
+++ 038-CombinationSum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    set<vector<int> > result;
+    void dfs(vector<int> &candidates, int target, int begin, vector<int> &vec){
+        for(int i = begin; i<candidates.size(); i++){
+            if(target==candidates[i]){
+                vec.push_back(candidates[i]);
+                result.insert(vec);
+                vec.pop_back();
+                return;
+            }
+            else if(target>candidates[i]){
+                vec.push_back(candidates[i]);
+                dfs(candidates, target-candidates[i], i,vec);
+            }
+            else
+                return;
+            vec.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        if(candidates.size())
+            sort(candidates.begin(), candidates.end());
+        result.clear();
+        vector<int> vec;
+        dfs(candidates, target, 0, vec);
+        vector<vector<int> > ans(result.begin(), result.end());
+        return ans;
+    }
+};
