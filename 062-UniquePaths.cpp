dcaf428a83a33e4a8f5725951d1e3f835b08d232
carriez0705 <carriez0705@gmail.com>
2015-11-17 00:07:30 +0800

    Update 062-UniquePaths.cpp

--- 062-UniquePaths.cpp
+++ 062-UniquePaths.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        if(!m) return 0;
-        vector<vector<int> > way(m, vector<int>(n,0));
-        for(int i = 0; i<m; i++)
-            way[i][0] = 1;
-        for(int j = 0; j<n; j++)
-            way[0][j] = 1;
-        for(int i = 1; i<m; i++)
-            for(int j = 1; j<n; j++){
-                way[i][j] = way[i-1][j]+way[i][j-1];
-            }
-        return way[m-1][n-1];
+       if(!m || !n) return 0;
+       vector<int> ans(n, 1);
+       for(int i = 1; i<m; i++){
+           for(int j = 0; j<n; j++){
+               int left = j==0? 0: ans[j-1];
+               ans[j] += left;
+           }
+       }
+       return ans[n-1];
     }
 };
