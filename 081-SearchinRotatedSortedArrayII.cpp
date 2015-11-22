0a37b549350a1ed993ddacf9345a4540952acdc5
carriez0705 <carriez0705@gmail.com>
2015-11-22 21:04:01 +0800

    Update 081-SearchinRotatedSortedArrayII.cpp

--- 081-SearchinRotatedSortedArrayII.cpp
+++ 081-SearchinRotatedSortedArrayII.cpp
@@ -35,3 +35,43 @@ public:
         return bst(nums, target, 0, nums.size()-1)!=-1;
     }
 };
+
+//non-recursive
+class Solution {
+    bool comp(vector<int> &nums, int left, int right){
+        if(left>=right) return true;
+       int mid = (left+right)>>1;
+       if(nums[mid]!=nums[left])
+            return false;
+        else
+            return comp(nums, left, mid-1) && comp(nums, mid+1, right);
+    }
+public:
+    bool search(vector<int>& nums, int target) {
+        int left=0, right = nums.size()-1;
+        while(left<=right){
+            int mid = (left+right)>>1;
+            if(nums[mid]==target)
+                return true;
+            if(nums[left]==nums[mid] && nums[mid]==nums[right]){
+                if(comp(nums, left, mid))
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+            else if(nums[left]<=nums[mid]){
+                if(target>=nums[left] && target<=nums[mid])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+            else{
+                if(target>=nums[mid] && target<=nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+        }
+        return false;
+    }
+};
