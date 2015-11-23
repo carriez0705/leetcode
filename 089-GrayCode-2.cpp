94aede6145a0912f9636667a31bb2e625f1bce1d
carriez0705 <carriez0705@gmail.com>
2015-11-24 00:14:22 +0800

    Update 089-GrayCode-2.cpp

--- 089-GrayCode-2.cpp
+++ 089-GrayCode-2.cpp
@@ -9,3 +9,25 @@ public:
         return vec;
     }
 };
+
+//
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> vec(pow(2,n), 0);
+        for(int t = 1; t<vec.size(); t++){
+            int lastAge = t-1;
+            for(int i = 0; i<n; i++){
+                int age = t>>i;
+                int lastBit = (vec[t-1]&(1<<i))>>i;
+                if(age==(lastAge>>i) || age%2==0){
+                    vec[t] |= lastBit<<i;
+                }
+                else{
+                    vec[t] |= (!lastBit)<<i;
+                }
+            }
+        }
+        return vec;
+    }
+};
