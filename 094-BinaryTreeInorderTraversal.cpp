c8a85579bc38f40ef334b63c33f6f416b18c5aa6
carriez0705 <carriez0705@gmail.com>
2015-11-26 22:28:36 +0800

    Update 094-BinaryTreeInorderTraversal.cpp

--- 094-BinaryTreeInorderTraversal.cpp
+++ 094-BinaryTreeInorderTraversal.cpp
@@ -10,6 +10,40 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> nodestack;
+        if(root)
+            nodestack.push(root);
+        TreeNode *node = root;
+        while(!nodestack.empty()){
+            while(node && node->left){
+                nodestack.push(node->left);
+                node = node->left;
+            }
+            if(!nodestack.empty()){
+                node = nodestack.top();
+                ans.push_back(node->val);
+                nodestack.pop();
+                node = node->right;
+                if(node) nodestack.push(node);
+            }
+        }
+        return ans;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
         stack<TreeNode*> nodstack;
         //why hash_map could not be used??
         map<TreeNode*, int> color;
