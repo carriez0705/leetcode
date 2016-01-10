1bd3e93d68fc4009023d84f28e9338cac2ce3ad3
carriez0705 <carriez0705@gmail.com>
2016-01-10 23:14:21 +0800

    Update 214-ShortestPalindrome.cpp

--- 214-ShortestPalindrome.cpp
+++ 214-ShortestPalindrome.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     string shortestPalindrome(string s) {
         int n = s.size();
-        if(n<2) return s;
-        int M = 26;
-        vector<vector<int> > dfa(M, vector<int>(n/2+1,0));
-        for(int k = 0; k<M; k++)
-            dfa[k][0] = 0;
-        dfa[s[0]-'a'][0] = 1;
+        if(!n) return s;
+        int i = 0, j = n-1;
+        int k = 26;
+        vector<vector<int> > dfa(k, vector<int>(1, 0));
         int X = 0;
-        int j = 0, i= n-1;
-        int cur = 0;
-        for(;i>j;i--){
-            if(j>cur){
-                for(int k = 0; k<M;k++)
-                    dfa[k][j] = dfa[k][X];
-                dfa[s[j]-'a'][j] = j+1;
-                X = dfa[s[j]-'a'][X];
-                cur = j;
+        dfa[s[0]-'a'][0] = 1;
+        while(i<j){
+            if(i+1>dfa[0].size()){
+                //continue construct dfa
+                for(int p = 0; p<k; p++){
+                    dfa[p].push_back(dfa[p][X]);
+                }
+                dfa[s[i]-'a'][i] = i+1;
+                X = dfa[s[i]-'a'][X];
             }
-            j = dfa[s[i]-'a'][j];
+            i = dfa[s[j]-'a'][i];
+            j--;
         }
         string sub = "";
-        if(j==0)
-            sub = s.substr(1,n-1);
-        else{
-            if(i==j) sub = s.substr(i+j+1, n-2*j-1);
-            else sub = s.substr(i+j+1, n-2*j);
-        }
-        int p = 0, q= sub.size()-1;
-        while(p<q){
-            swap(sub[p], sub[q]);
-            p++;
-            q--;
+        if(i+j+1<n)
+        {sub = s.substr(i+j+1);
+        reverse(sub.begin(), sub.end());
         }
         return sub+s;
     }
